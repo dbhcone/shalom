74bc2f79d38abe3f6e1557e309d5d1b4e94ced9b
Enoch <enoch@getinnotized.com>
2021-08-11 16:52:56 +0000

    removes expired edit member component

--- src/app/components/admin/members/members.component.ts
+++ src/app/components/admin/members/members.component.ts
@@ -4,7 +4,6 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog } from '@angular/material/dialog';
 import { AddMemberComponent } from './add-member.component';
-import { EditMemberComponent } from './edit-member.component';
 import { AuthService } from 'src/app/services/auth.service';
 import { Subscription } from 'rxjs';
 import Swal from 'sweetalert2';
