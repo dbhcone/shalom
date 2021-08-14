29f8124d0395f0a5ea828fd6f452780ca069886d
Enoch <enoch@getinnotized.com>
2021-08-14 13:16:20 +0000

    user helper class modified

--- src/app/helpers/functions/user.helper.ts
+++ src/app/helpers/functions/user.helper.ts
@@ -2,25 +2,25 @@ export class UserAccountHelper {
   generateUsername(name: Name): string {
     let generatedname = '';
     const { firstName, surname, otherNames } = name;
-    if (firstName?.trim() != '' && surname?.trim() != '' && otherNames?.trim() != '') {
+    if (firstName && surname && otherNames) {
       generatedname = `${firstName}${otherNames.substring(
         0,
         1
-      )}${surname.substring(0, 3)}`;
-    } else if (firstName?.trim() != '' && surname?.trim() != '') {
-      generatedname = `${firstName}${surname.substring(0, 3)}`;
+      )}.${surname}`;
+    } else if (firstName && surname) {
+      generatedname = `${firstName}.${surname}`;
     }
     return generatedname.toLowerCase();
   }
 
   getFullName(name: Name) : string {
     const {firstName, surname, otherNames} = name;
-    return `${surname.toUpperCase()} ${otherNames ?.substr(0,1).toUpperCase() }. ${firstName }`
+    return `${surname?.toUpperCase()} ${otherNames ?.substr(0,1).toUpperCase() }. ${firstName }`
   }
 }
 
 interface Name {
-  surname: string;
-  firstName: string;
-  otherNames: string;
+  surname?: string;
+  firstName?: string;
+  otherNames?: string;
 }
