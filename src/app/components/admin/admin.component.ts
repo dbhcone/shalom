f29f3d16359ffdb1c69270a82c2b3761a09bf085
Enoch <enoch@getinnotized.com>
2021-08-16 01:12:07 +0000

    admin dashboard handles side-nav autoclose on handset

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -2,20 +2,24 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+    selector: 'app-admin',
+    templateUrl: './admin.component.html',
+    styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
+    isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+        map((result) => result.matches),
+        shareReplay(),
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+    constructor(private breakpointObserver: BreakpointObserver) {}
 
+    handleClick(drawer: MatSidenav) {
+        this.isHandset$.subscribe((ishandset) => {
+            ishandset && drawer.toggle();
+        });
+    }
 }
